248892f52e1aa20f3d19718fa2cec45e7facb0e2
navtsingh <navtsingh@ladmins-MacBook-Pro.local>
2022-09-13 17:03:26 -0700

    templates moved outside

--- src/templates/node-cli-typescript/dist/cli.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cli = void 0;
-const addTwo_1 = require("./utils/addTwo");
-const cli = () => {
-    console.log((0, addTwo_1.addTwo)(2));
-};
-exports.cli = cli;
--- src/templates/node-cli-typescript/dist/utils/addTwo.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addTwo = void 0;
-function addTwo(a) {
-    return a + 2;
-}
-exports.addTwo = addTwo;
