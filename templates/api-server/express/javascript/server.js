253dd29869c69bcc357633315ec4a10579ad6f31
navtsingh <navtsingh@ladmins-MacBook-Pro-3.local>
2023-08-01 22:43:43 -0700

    added

--- templates/api-server/express/javascript/server.js
+++ templates/api-server/express/javascript/server.js
@@ -1,7 +1,7 @@
 const {app} = require("./src/app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-app.listen("3000", () => {
+app.listen(`${PORT}`, () => {
     console.log(`server is listening on port ${PORT}`)
 })
